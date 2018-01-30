de9778a3d43511ca4ce27daa53ee24f3826421d4
carlosdelreal <csdelreal@gmail.com>
2018-01-30 00:24:49 -0600

    gh-pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
+const repo = `/${window.location.pathname.split('/')[1]}`;
 const Header = (props) => {
 	// stateless functional component
 	return (
 		<header className="header">
-			<a href="/" className="header__title"><h1>Guitar Warehouse</h1></a>
+			<a href={repo} className="header__title"><h1>Guitar Warehouse</h1></a>
 			<h2 className="header__tagline">{props.tagline}</h2>
 		</header>
 	)
